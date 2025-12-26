294ec69fa4ce35541a3ed0b9f05035101967eaef
lishuhang <49574+lishuhang@users.noreply.github.com>
2025-12-26 12:49:01 +0800

    integrate css/js

--- assets/gallery.js
+++
@@ -1,8 +0,0 @@
-        function toggleImages(element) {
-            var imagesDiv = element.nextElementSibling;
-            if (imagesDiv.style.display === "none" || imagesDiv.style.display === "") {
-                imagesDiv.style.display = "block";
-            } else {
-                imagesDiv.style.display = "none";
-            }
-        }
